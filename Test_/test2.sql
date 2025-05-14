966dd50c6cfd5f3707acaec4d32747008f0d8755
TomaszK94 <pythonbotw3c@gmail.com>
2025-05-14 19:19:05 +0200

    test_3

--- Test_/test2.sql
+++ Test_/test2.sql
@@ -1,7 +1,18 @@
-SELECT *
+SELECT 
+    s.orderdate,
+    s.quantity * s.netprice * s.exchangerate AS net_revenue,
+    c.givenname,
+    c.surname,
+    c.countryfull,
+    c.continent,
+    p.productkey,
+    p.productname,
+    p.categoryname,
+    p.subcategoryname
 FROM 
     sales AS s
 LEFT JOIN customer AS c on s.customerkey = c.customerkey
 LEFT JOIN product AS p on s.productkey = p.productkey
 WHERE
     s.orderdate::date >= '2020-01-01'
+LIMIT 100;
