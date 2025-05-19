9b2a678dcca1154e506feca1315a7257d466846e
TomaszK94 <pythonbotw3c@gmail.com>
2025-05-19 17:53:07 +0200

    Case_statment_5

--- Pivot_with_Case/Aggregation.sql
+++ Pivot_with_Case/Aggregation.sql
@@ -1,7 +1,9 @@
 SELECT 
     sales.orderdate,
     COUNT(DISTINCT sales.customerkey) AS total_customer,
-    COUNT(DISTINCT CASE WHEN customer.continent = 'Europe' THEN sales.customerkey END) AS eu_customers
+    COUNT(DISTINCT CASE WHEN customer.continent = 'Europe' THEN sales.customerkey END) AS eu_customers,
+    COUNT(DISTINCT CASE WHEN customer.continent = 'North America' THEN sales.customerkey END) AS na_customers,
+    COUNT(DISTINCT CASE WHEN customer.continent = 'Australia' THEN sales.customerkey END) AS au_customers
 FROM 
     sales
 LEFT JOIN customer ON customer.customerkey = sales.customerkey
