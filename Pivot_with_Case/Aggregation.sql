198859dc1b19e66d4688ed3d84dd0fc2f56500c9
TomaszK94 <pythonbotw3c@gmail.com>
2025-05-19 17:45:22 +0200

    Case_statment_3

--- Pivot_with_Case/Aggregation.sql
+++ Pivot_with_Case/Aggregation.sql
@@ -1,8 +1,10 @@
 SELECT 
     orderdate,
-    COUNT(customerkey) AS total_customer
+    COUNT(DISTINCT customerkey) AS total_customer
 FROM 
     sales
+WHERE
+    orderdate BETWEEN '2023-01-01' AND '2023-12-31'
 GROUP BY
     orderdate
 ORDER BY
