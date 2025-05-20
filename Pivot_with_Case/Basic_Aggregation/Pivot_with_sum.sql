30d6df511d7577cbae27cdd65840f98c4c7b4e8f
TomaszK94 <pythonbotw3c@gmail.com>
2025-05-20 19:38:02 +0200

    pivot_with_sum_2

--- Pivot_with_Case/Basic_Aggregation/Pivot_with_sum.sql
+++ Pivot_with_Case/Basic_Aggregation/Pivot_with_sum.sql
@@ -22,3 +22,18 @@ GROUP BY
     product.categoryname
 ORDER BY
     product.categoryname;
+
+
+
+SELECT
+    product.categoryname,
+    SUM(sales.quantity * sales.netprice * sales.exchangerate) AS net_revenue,
+    SUM(CASE WHEN sales.orderdate BETWEEN '2022-01-01' AND '2022-12-31' THEN sales.quantity * sales.netprice * sales.exchangerate ELSE 0 END) AS total_revenue_2022,
+    SUM(CASE WHEN sales.orderdate BETWEEN '2023-01-01' AND '2023-12-31' THEN sales.quantity * sales.netprice * sales.exchangerate ELSE 0 END) AS total_revenue_2023
+FROM
+    sales
+LEFT JOIN product ON sales.productkey = product.productkey
+GROUP BY
+    product.categoryname
+ORDER BY
+    product.categoryname;
