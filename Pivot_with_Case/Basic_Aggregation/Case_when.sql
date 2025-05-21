50195b775fc88d8b232f410c45fb463c6da5c3db
TomaszK94 <pythonbotw3c@gmail.com>
2025-05-21 13:59:04 +0200

    case_when

---
+++ Pivot_with_Case/Basic_Aggregation/Case_when.sql
@@ -0,0 +1,18 @@
+SELECT 
+    s.orderdate,
+    s.quantity * s.netprice * s.exchangerate AS net_revenue,
+    CASE WHEN s.quantity * s.netprice * s.exchangerate >= 1000 THEN 'HIGH' ELSE 'LOW' END AS revenue_rank,
+    c.givenname,
+    c.surname,
+    c.countryfull,
+    c.continent,
+    p.productkey,
+    p.productname,
+    p.categoryname,
+    p.subcategoryname
+FROM 
+    sales s
+LEFT JOIN customer c ON s.customerkey = c.customerkey
+LEFT JOIN product p ON s.productkey = p.productkey
+WHERE
+    orderdate::date >= '2020-01-01'
