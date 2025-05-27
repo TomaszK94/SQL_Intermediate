c40a6ebf00d0e18350c94d19c6ef566bc4834cff
TomaszK94 <pythonbotw3c@gmail.com>
2025-05-27 19:43:06 +0200

    Case_when_median_3

--- 2_Pivot_with_Case/Statistical_Aggregation/Case_when_median.sql
+++ 2_Pivot_with_Case/Statistical_Aggregation/Case_when_median.sql
@@ -21,7 +21,10 @@ SELECT
     product.categoryname,
     PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY (CASE 
         WHEN sales.orderdate BETWEEN '2022-01-01' AND '2022-12-31' THEN (sales.quantity * sales.netprice * sales.exchangerate) 
-    END)) AS median_sales_2022
+    END)) AS median_sales_2022,
+    PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY (CASE 
+        WHEN sales.orderdate BETWEEN '2023-01-01' AND '2023-12-31' THEN (sales.quantity * sales.netprice * sales.exchangerate) 
+    END)) AS median_sales_2023
 FROM
     sales
 LEFT JOIN product ON sales.productkey = product.productkey
