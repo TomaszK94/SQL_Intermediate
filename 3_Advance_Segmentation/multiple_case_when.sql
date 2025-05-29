b28f14a559fca858d13464d7a3e9f976bd08a5a5
TomaszK94 <pythonbotw3c@gmail.com>
2025-05-29 15:43:12 +0200

    Advance_segmentation5

--- 3_Advance_Segmentation/multiple_case_when.sql
+++ 3_Advance_Segmentation/multiple_case_when.sql
@@ -10,3 +10,27 @@ SELECT
 FROM 
     table_name;
 */
+
+/* CATEGORIZE
+
+- Mutliple High Value Items --> quantity >=2 AND  netprice >= 100
+- Single High Value Item --> netprice >= 100
+- Multiple Standard Items --> quantity >= 2 
+- Single Standard Item --> Otherwise 
+
+*/
+
+
+SELECT 
+    orderdate,
+    quantity,
+    netprice,
+    CASE 
+        WHEN quantity >=2 AND netprice >= 100 THEN 'Mutliple High Value Items'
+        WHEN netprice >= 100 THEN 'Single High Value Item'
+        WHEN quantity >=2 THEN 'Multiple Standard Items'
+        ELSE 'Single Standard Item' 
+    END AS order_value
+FROM
+    sales
+LIMIT 50;
