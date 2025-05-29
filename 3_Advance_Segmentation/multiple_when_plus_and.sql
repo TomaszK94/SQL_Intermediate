ad6016e494a0709cedab6bf4a5ea4f5ad2355f34
TomaszK94 <pythonbotw3c@gmail.com>
2025-05-29 16:08:28 +0200

    Advance_segmentation7

--- 3_Advance_Segmentation/multiple_when_plus_and.sql
+++ 3_Advance_Segmentation/multiple_when_plus_and.sql
@@ -26,3 +26,39 @@ GROUP BY
     product.categoryname
 ORDER BY
     product.categoryname; 
+
+
+-- Without "Hard Coding" --> USING CTE
+
+WITH median_value_2022_2023 AS (
+    SELECT
+        PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY (s.quantity * s.netprice * s.exchangerate)) AS median
+    FROM 
+    sales AS s 
+    WHERE
+        orderdate BETWEEN '2022-01-01' AND '2023-12-31'
+)
+
+SELECT
+    product.categoryname,
+    SUM(CASE WHEN (sales.quantity * sales.netprice * sales.exchangerate) < median
+            AND sales.orderdate BETWEEN '2022-01-01' AND '2022-12-31'
+        THEN (sales.quantity * sales.netprice * sales.exchangerate) END) AS low_net_revenue_2022,
+    SUM(CASE WHEN (sales.quantity * sales.netprice * sales.exchangerate) >= median 
+            AND sales.orderdate BETWEEN '2022-01-01' AND '2022-12-31'
+        THEN (sales.quantity * sales.netprice * sales.exchangerate) END) AS high_net_revenue_2022,
+    SUM(CASE WHEN (sales.quantity * sales.netprice * sales.exchangerate) < median 
+            AND sales.orderdate BETWEEN '2023-01-01' AND '2023-12-31'
+        THEN (sales.quantity * sales.netprice * sales.exchangerate) END) AS low_net_revenue_2023,
+    SUM(CASE WHEN (sales.quantity * sales.netprice * sales.exchangerate) >= median 
+            AND sales.orderdate BETWEEN '2023-01-01' AND '2023-12-31'
+        THEN (sales.quantity * sales.netprice * sales.exchangerate) END) AS high_net_revenue_2023
+FROM
+    sales
+LEFT JOIN 
+    product ON sales.productkey = product.productkey,
+    median_value_2022_2023 AS median
+GROUP BY
+    product.categoryname
+ORDER BY
+    product.categoryname; 
