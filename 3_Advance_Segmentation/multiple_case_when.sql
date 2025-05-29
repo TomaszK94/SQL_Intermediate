19c15fceb64143251e132dccd5a24eeb1b6ea8b2
TomaszK94 <pythonbotw3c@gmail.com>
2025-05-29 15:37:42 +0200

    Advance_segmentation4

---
+++ 3_Advance_Segmentation/multiple_case_when.sql
@@ -0,0 +1,12 @@
+/* 
+Multiple WHEN Clauses in a single Case Block:
+
+SELECT 
+    CASE
+        WHEN contidion1 THEN column
+        WHEN condition2 THEN column
+        ELSE column
+    END AS alias
+FROM 
+    table_name;
+*/
