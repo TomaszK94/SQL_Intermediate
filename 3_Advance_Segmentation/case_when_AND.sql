5cae501ffc41e547162a05d090b7f3254cdd1ea3
TomaszK94 <pythonbotw3c@gmail.com>
2025-05-29 15:32:55 +0200

    Advance_segmentation_2

--- 3_Advance_Segmentation/case_when_AND.sql
+++ 3_Advance_Segmentation/case_when_AND.sql
@@ -7,3 +7,15 @@ FROM
     table_name;
 */
 
+
+-- "High Value Order" if quantity >=2 AND netprice >=50 ELSE Standard Order
+
+SELECT 
+    orderdate,
+    quantity,
+    netprice,
+    CASE WHEN quantity >=2 AND netprice >= 50 THEN 'High Value Order'
+        ELSE 'Standard Order' END AS order_value
+FROM
+    sales
+LIMIT 10;
