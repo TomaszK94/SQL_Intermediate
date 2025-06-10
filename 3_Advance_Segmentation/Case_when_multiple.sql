3b581e16cc3db8ddeaa855d29c21170f23a13ec3
TomaszK94 <pythonbotw3c@gmail.com>
2025-06-10 17:47:25 +0200

    multiple_case_when_2

--- 3_Advance_Segmentation/Case_when_multiple.sql
+++ 3_Advance_Segmentation/Case_when_multiple.sql
@@ -1,6 +1,10 @@
-SELECT
-    PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY (s.quantity * s.netprice * s.exchangerate)) AS perc_25th
-FROM 
-sales AS s 
-WHERE
-    orderdate BETWEEN '2022-01-01' AND '2023-12-31'
+
+WITH percentile AS (
+    SELECT
+        PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY (s.quantity * s.netprice * s.exchangerate)) AS perc_25th,
+        PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY (s.quantity * s.netprice * s.exchangerate)) AS perc_75th
+    FROM 
+        sales AS s 
+    WHERE
+        orderdate BETWEEN '2022-01-01' AND '2023-12-31'
+)
