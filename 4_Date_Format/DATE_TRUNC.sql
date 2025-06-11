dec04770d03e45350f5bf958ed54618c5821fdcf
TomaszK94 <pythonbotw3c@gmail.com>
2025-06-11 23:30:42 +0200

    DATE_TRUNC_3

--- 4_Date_Format/DATE_TRUNC.sql
+++ 4_Date_Format/DATE_TRUNC.sql
@@ -9,8 +9,18 @@ LIMIT 10;
 
 SELECT 
     orderdate,
-    DATE_TRUNC('month', orderdate)::DATE
+    DATE_TRUNC('month', orderdate)::DATE AS order_month
 FROM
     sales
 ORDER BY RANDOM()
+LIMIT 10;
+
+
+SELECT 
+    DATE_TRUNC('month', orderdate)::DATE AS order_month,
+    SUM(netprice * quantity * exchangerate) AS net_revenue
+FROM
+    sales
+GROUP BY
+    order_month
 LIMIT 10;
