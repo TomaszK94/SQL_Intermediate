7da09a114b322a4f7c4cb33a4398f884120f284e
TomaszK94 <pythonbotw3c@gmail.com>
2025-06-13 18:15:44 +0200

    CURRENT_DATE_2

--- 4_Date_Format/CURRENT_DATE+NOW().sql
+++ 4_Date_Format/CURRENT_DATE+NOW().sql
@@ -1,2 +1,27 @@
 SELECT 
     CURRENT_DATE
+
+SELECT 
+    NOW()
+
+
+
+
+SELECT
+    CURRENT_DATE,
+    EXTRACT(YEAR FROM orderdate) AS order_year,
+    EXTRACT(YEAR FROM CURRENT_DATE) AS current_year,
+    EXTRACT(YEAR FROM CURRENT_DATE) - 5 AS five_years,
+    sales.orderdate,
+    product.categoryname,
+    SUM(sales.quantity * sales.netprice * sales.exchangerate) AS net_revenue
+FROM
+    sales
+LEFT JOIN 
+    product ON sales.productkey = product.productkey
+GROUP BY
+    sales.orderdate,
+    product.categoryname
+ORDER BY
+    sales.orderdate,
+    product.categoryname
