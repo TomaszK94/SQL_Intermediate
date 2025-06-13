5f5114edd89b8a6be98f48c21dacc3d836047187
TomaszK94 <pythonbotw3c@gmail.com>
2025-06-13 17:49:01 +0200

    TO_CHAR_4

--- 4_Date_Format/TO_CHAR.sql
+++ 4_Date_Format/TO_CHAR.sql
@@ -16,3 +16,16 @@ FROM
 ORDER BY
     RANDOM()
 LIMIT 10;
+
+
+
+SELECT 
+    TO_CHAR(orderdate,'YYYY-MM') AS order_month,
+    SUM(netprice * quantity * exchangerate) AS net_revenue,
+    COUNT(DISTINCT customerkey) AS unique_customers
+FROM
+    sales
+WHERE
+    orderdate BETWEEN '2023-01-01' AND '2024-12-31'
+GROUP BY
+    order_month;
