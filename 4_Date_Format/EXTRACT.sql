1102e7813e0ee398e2672414b600b0be15b79426
TomaszK94 <pythonbotw3c@gmail.com>
2025-06-13 18:04:01 +0200

    EXTRACT_3

--- 4_Date_Format/EXTRACT.sql
+++ 4_Date_Format/EXTRACT.sql
@@ -18,3 +18,20 @@ FROM
 ORDER BY
     RANDOM()
 LIMIT 10;
+
+
+SELECT 
+    EXTRACT(YEAR FROM orderdate) AS order_year,
+    EXTRACT(MONTH FROM orderdate) AS order_month,
+    SUM(netprice * quantity * exchangerate) AS net_revenue,
+    COUNT(DISTINCT customerkey) AS unique_customers
+FROM
+    sales
+WHERE
+    orderdate BETWEEN '2022-01-01' AND '2024-12-31'
+GROUP BY
+    order_year,
+    order_month
+ORDER BY
+    order_year,
+    order_month;
