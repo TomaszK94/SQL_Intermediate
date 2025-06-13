6e88f7265c5b10d6e5276b1c8822966466bd2c15
TomaszK94 <pythonbotw3c@gmail.com>
2025-06-13 17:54:59 +0200

    DATE_PART_2

--- 4_Date_Format/DATE_PART.sql
+++ 4_Date_Format/DATE_PART.sql
@@ -6,3 +6,15 @@ DATE_PART('UNIT', source)
 
 DATE_PART('year', orderdate)
 */
+
+
+SELECT
+    orderdate,
+    DATE_PART('year', orderdate) AS order_year,
+    DATE_PART('month', orderdate) AS order_month,
+    DATE_PART('day', orderdate) AS order_day
+FROM
+    sales
+ORDER BY
+    RANDOM()
+LIMIT 10;
