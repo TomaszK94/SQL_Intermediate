2fefa3c0853d5148ce1abe7aaaed3a647784273f
TomaszK94 <pythonbotw3c@gmail.com>
2025-06-23 19:00:17 +0200

    AGE_EXTRACT_3

--- 4_Date_Format/AGE_EXTRACT.sql
+++ 4_Date_Format/AGE_EXTRACT.sql
@@ -6,3 +6,12 @@ SELECT AGE('2025-01-23', '2025-01-01')
 SELECT
 EXTRACT(DAY FROM AGE('2025-06-23', '2025-01-01')) - 10
 
+
+SELECT
+    orderdate,
+    deliverydate,
+    AGE(deliverydate, orderdate) AS processing_time
+FROM
+    sales
+ORDER BY RANDOM()
+LIMIT 20;
