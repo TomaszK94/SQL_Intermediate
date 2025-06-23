16293ce78b67c0ea5f93e7f552fb47cacba3f36c
TomaszK94 <pythonbotw3c@gmail.com>
2025-06-23 18:57:05 +0200

    AGE_EXTRACT_2

--- 4_Date_Format/AGE_EXTRACT.sql
+++ 4_Date_Format/AGE_EXTRACT.sql
@@ -1,4 +1,8 @@
 -- EXAMPLE
-SELECT AGE('2025-06-23', '2025-01-01')
+SELECT AGE('2025-01-23', '2025-01-01')
+-- AGE --> Interval we cannot make any operation likes + - 
 
+
+SELECT
+EXTRACT(DAY FROM AGE('2025-06-23', '2025-01-01')) - 10
 
