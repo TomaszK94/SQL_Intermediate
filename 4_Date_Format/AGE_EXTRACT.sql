71309b93802c3e2bff440f0e5e8b6cd7ffbddff0
TomaszK94 <pythonbotw3c@gmail.com>
2025-06-23 19:05:57 +0200

    AGE_EXTRACT_5

--- 4_Date_Format/AGE_EXTRACT.sql
+++ 4_Date_Format/AGE_EXTRACT.sql
@@ -9,10 +9,8 @@ EXTRACT(DAY FROM AGE('2025-06-23', '2025-01-01')) - 10
 
 SELECT
     DATE_PART('year', orderdate) AS order_year,
-    orderdate,
-    deliverydate,
-    AGE(deliverydate, orderdate) AS processing_time
+    AVG(EXTRACT(DAY FROM AGE(deliverydate, orderdate))) AS avg_processing_time
 FROM
     sales
-ORDER BY RANDOM()
-LIMIT 20;
+GROUP BY 
+    order_year;
