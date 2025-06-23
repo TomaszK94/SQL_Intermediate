87fa6950ce0102b54998c100a87f2bd625030cd2
TomaszK94 <pythonbotw3c@gmail.com>
2025-06-23 19:12:32 +0200

    AGE_EXTRACT_8

--- 4_Date_Format/AGE_EXTRACT.sql
+++ 4_Date_Format/AGE_EXTRACT.sql
@@ -9,7 +9,8 @@ EXTRACT(DAY FROM AGE('2025-06-23', '2025-01-01')) - 10
 
 SELECT
     DATE_PART('year', orderdate) AS order_year,
-    ROUND(AVG(EXTRACT(DAY FROM AGE(deliverydate, orderdate))),2) AS avg_processing_time
+    ROUND(AVG(EXTRACT(DAY FROM AGE(deliverydate, orderdate))),2) AS avg_processing_time,
+    ROUND(SUM(quantity * netprice * exchangerate)) AS net_revenue
 FROM
     sales
 WHERE
