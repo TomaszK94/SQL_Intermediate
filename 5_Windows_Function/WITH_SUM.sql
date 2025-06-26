c1a5f5e248f28dd3da11879e2da253b46f4a1a37
TomaszK94 <pythonbotw3c@gmail.com>
2025-06-26 22:27:49 +0200

    WINDOW_FUNCTION_SUM_1

--- 5_Windows_Function/WITH_SUM.sql
+++ 5_Windows_Function/WITH_SUM.sql
@@ -1,7 +1,6 @@
 SELECT
     orderdate,
-    orderkey,
-    linenumber,
+    orderkey * 10 + linenumber AS order_line_number,
     (quantity * netprice * exchangerate) AS net_revenue
 FROM
     sales
