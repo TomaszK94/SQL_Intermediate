93fd30c3f6fc62fe0b48bf8211c801018031f5f1
TomaszK94 <pythonbotw3c@gmail.com>
2025-06-26 22:29:27 +0200

    WINDOW_FUNCTION_SUM_2

--- 5_Windows_Function/WITH_SUM.sql
+++ 5_Windows_Function/WITH_SUM.sql
@@ -1,7 +1,8 @@
 SELECT
     orderdate,
     orderkey * 10 + linenumber AS order_line_number,
-    (quantity * netprice * exchangerate) AS net_revenue
+    (quantity * netprice * exchangerate) AS net_revenue,
+    SUM(quantity * netprice * exchangerate) OVER(PARTITION BY orderdate) AS daily_revenue
 FROM
     sales
-LIMIT 20;
+LIMIT 100;
