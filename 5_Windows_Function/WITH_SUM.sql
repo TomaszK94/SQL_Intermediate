9f10f9e659ea42f2ed0d86ebff3ec51c498c7dcb
TomaszK94 <pythonbotw3c@gmail.com>
2025-06-26 22:45:19 +0200

    WINDOW_FUNCTION_SUM_5

--- 5_Windows_Function/WITH_SUM.sql
+++ 5_Windows_Function/WITH_SUM.sql
@@ -10,3 +10,21 @@ ORDER BY
     orderdate,
     percent_daily DESC
 LIMIT 100;
+
+-- ORGANIZED BY CTE USAGE
+
+SELECT *,
+    ROUND((100 * net_revenue::numeric / daily_revenue::numeric), 2) AS daily_percent
+FROM(
+    SELECT
+        orderdate,
+        orderkey * 10 + linenumber AS order_line_number,
+        (quantity * netprice * exchangerate) AS net_revenue,
+        SUM(quantity * netprice * exchangerate) OVER(PARTITION BY orderdate) AS daily_revenue
+    FROM
+        sales
+) AS revenue_by_day
+ORDER BY
+    orderdate,
+    daily_percent DESC
+LIMIT 100;
