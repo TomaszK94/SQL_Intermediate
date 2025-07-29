31f8148c9bd5f5da909cb58eb33cf4148e3e7034
TomaszK94 <pythonbotw3c@gmail.com>
2025-07-29 20:45:35 +0200

    GROUP__BY&WINDOW_FUNCTIONS_1

--- 5_Windows_Function/Window_Functions&Group_By.sql
+++ 5_Windows_Function/Window_Functions&Group_By.sql
@@ -1,5 +1,14 @@
 SELECT
     customerkey,
+    AVG(quantity * netprice * exchangerate) AS net_revenue,
     COUNT(*) OVER (PARTITION BY customerkey) AS total_order
 FROM
     sales
+GROUP BY
+    customerkey
+
+/*
+WINDOW FUNCTIONS RUN AFTER GROUP BY !!!
+NOT RECOMENDED COMBINE THEM.
+BETTER USE CTE FIRST TO APPLY WINDOW FUNCTION THEN PERFORM GROUP BY
+*/
