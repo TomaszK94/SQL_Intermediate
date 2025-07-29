edb1bf8141766d30a28463dcf127336237ce6b61
TomaszK94 <pythonbotw3c@gmail.com>
2025-07-29 20:49:01 +0200

    GROUP_BY&WINDOW_FUNCTIONS_2

--- 5_Windows_Function/Window_Functions&Group_By.sql
+++ 5_Windows_Function/Window_Functions&Group_By.sql
@@ -12,3 +12,19 @@ WINDOW FUNCTIONS RUN AFTER GROUP BY !!!
 NOT RECOMENDED COMBINE THEM.
 BETTER USE CTE FIRST TO APPLY WINDOW FUNCTION THEN PERFORM GROUP BY
 */
+
+-- FIX PREVIOUS QUERY
+WITH customer_order AS(
+SELECT
+    customerkey,
+    (quantity * netprice * exchangerate) AS order_value,
+    COUNT(*) OVER (PARTITION BY customerkey) AS total_order
+FROM
+    sales
+)
+
+SELECT 
+    customerkey,
+    total_order
+FROM
+    customer_order
