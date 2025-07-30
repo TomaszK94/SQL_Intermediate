ba8fa112ef37e7455c77b3a0bf54ca8f0b11bc72
TomaszK94 <pythonbotw3c@gmail.com>
2025-07-30 20:41:44 +0200

    Cohort_AVG_2

--- 5_Windows_Function/Cohort_Analyst_AVG.sql
+++ 5_Windows_Function/Cohort_Analyst_AVG.sql
@@ -8,3 +8,17 @@ FROM
     table_name
 
 */
+
+-- LTV --> Custome Lifetime Value
+
+
+SELECT
+    customerkey,
+    EXTRACT(YEAR FROM MIN(orderdate)) AS cohort_year,
+    SUM(quantity * netprice * exchangerate) AS customer_ltv
+FROM 
+    sales
+GROUP BY
+    customerkey
+ORDER BY 
+    customerkey
