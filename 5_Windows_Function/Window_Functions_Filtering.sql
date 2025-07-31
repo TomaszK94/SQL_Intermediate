80e1a53e2b8d98612ab28debce60a94ccae4ddef
TomaszK94 <pythonbotw3c@gmail.com>
2025-07-31 22:57:28 +0200

    W_F_FILTERING_3

--- 5_Windows_Function/Window_Functions_Filtering.sql
+++ 5_Windows_Function/Window_Functions_Filtering.sql
@@ -14,3 +14,11 @@ WHERE
     condition --> Filters data BEFORE applying window function
 
 */
+
+SELECT
+    customerkey,
+    EXTRACT(YEAR FROM MIN(orderdate) OVER (PARTITION BY customerkey)) AS cohort_year
+FROM 
+    sales
+WHERE 
+    orderdate >= '2020-01-01'
