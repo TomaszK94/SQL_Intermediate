a0f074aa4dab626d47071bb8e5c67a2f6975a8af
TomaszK94 <pythonbotw3c@gmail.com>
2025-07-31 22:53:16 +0200

    W_F_FILTERING_2

--- 5_Windows_Function/Window_Functions_Filtering.sql
+++ 5_Windows_Function/Window_Functions_Filtering.sql
@@ -1,3 +1,16 @@
 /*
 Filtering Before Windows Function
 Use WHERE to filter rows before aggregation.
+
+Syntax:
+
+SELECT
+    column name,
+    window_function(column_to_aggregate) 
+        OVER (PARTITION BY partition_column ORDER BY order_column) AS window_column_allias
+FROM 
+    table_name
+WHERE
+    condition --> Filters data BEFORE applying window function
+
+*/
