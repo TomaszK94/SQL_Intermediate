0c7c708ba17ed265e72a284cae8ce501920d2afc
TomaszK94 <pythonbotw3c@gmail.com>
2025-07-31 23:01:06 +0200

    W_F_FILTERING_4

--- 5_Windows_Function/Window_Functions_Filtering.sql
+++ 5_Windows_Function/Window_Functions_Filtering.sql
@@ -22,3 +22,26 @@ FROM
     sales
 WHERE 
     orderdate >= '2020-01-01'
+
+/*
+Filtering After Window Function
+USE a SUBQUERY + WHERE to filter based on window function results.
+
+Syntax:
+
+WITH windowed_data AS(
+    SELECT
+        column name,
+        window_function(column_to_aggregate)
+            OVER (PARTITION BY partition_column) AS window_column_allias
+    FROM
+        table_name
+)
+
+SELECT *
+FROM
+    windowed_data
+WHERE
+    window_column_allias condition --> Filters data after window function
+
+*/
