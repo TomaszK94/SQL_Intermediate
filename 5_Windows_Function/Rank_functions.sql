810e36a456014cce53a0d3e5a808667cb86fd34a
TomaszK94 <pythonbotw3c@gmail.com>
2025-08-03 22:45:08 +0200

    Rank_functions_4

--- 5_Windows_Function/Rank_functions.sql
+++ 5_Windows_Function/Rank_functions.sql
@@ -2,7 +2,8 @@ SELECT
     customerkey,
     COUNT(*) AS total_orderds,
     ROW_NUMBER() OVER(ORDER BY COUNT(*) DESC) AS total_orders_row_num,
-    RANK() OVER(ORDER BY COUNT(*) DESC) AS total_orders_rank
+    RANK() OVER(ORDER BY COUNT(*) DESC) AS total_orders_rank,
+    DENSE_RANK() OVER(ORDER BY COUNT(*) DESC) AS total_orders_dense_rank
 FROM
     sales
 GROUP BY
