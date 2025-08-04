ed563070cce11c12c881461f746aeb7f8d009362
TomaszK94 <pythonbotw3c@gmail.com>
2025-08-04 23:03:46 +0200

    LAG_LEAD_8_monthly_revenue_grow

--- 5_Windows_Function/LAG_LEAD.sql
+++ 5_Windows_Function/LAG_LEAD.sql
@@ -21,6 +21,7 @@ FROM
     monthly_revenue
 
 
+
 -- LAG() AND LEAD()
 WITH monthly_revenue AS(
     SELECT 
@@ -40,5 +41,29 @@ SELECT
     *,
     LAG(net_revenue) OVER(ORDER BY month) AS previous_month_revenue,
     LEAD(net_revenue) OVER(ORDER BY month) AS next_month_revenue
+FROM 
+    monthly_revenue
+
+
+
+-- MoM Growth
+WITH monthly_revenue AS(
+    SELECT 
+        TO_CHAR(orderdate, 'YYYY-MM') AS month,
+        SUM(exchangerate * quantity * netprice) AS net_revenue
+    FROM
+        sales
+    WHERE
+        EXTRACT(YEAR FROM orderdate) = 2023
+    GROUP BY 
+        month
+    ORDER BY
+        month
+)
+
+SELECT
+    *,
+    LAG(net_revenue) OVER(ORDER BY month) AS previous_month_revenue,
+    net_revenue - LAG(net_revenue) OVER(ORDER BY month) AS monthly_revenue_grow
 FROM 
     monthly_revenue
