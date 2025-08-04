fe117bedbdf76514712ff248dbfd079cafbaa868
TomaszK94 <pythonbotw3c@gmail.com>
2025-08-04 22:53:11 +0200

    LAG_LEAD_6_LAG()

--- 5_Windows_Function/LAG_LEAD.sql
+++ 5_Windows_Function/LAG_LEAD.sql
@@ -19,3 +19,25 @@ SELECT
     NTH_VALUE(net_revenue, 3) OVER(ORDER BY month ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS third_month_revenue
 FROM 
     monthly_revenue
+
+
+-- LAG() AND LEAD()
+WITH monthly_revenue AS(
+    SELECT 
+        TO_CHAR(orderdate, 'YYYY-MM') AS month,
+        SUM(exchangerate * quantity * netprice) AS net_revenue
+    FROM
+        sales
+    WHERE
+        EXTRACT(YEAR FROM orderdate) = 2023
+    GROUP BY 
+        month
+    ORDER BY
+        month
+)
+
+SELECT
+    *,
+    LAG(net_revenue) OVER(ORDER BY month) AS previous_month_revenue
+FROM 
+    monthly_revenue
