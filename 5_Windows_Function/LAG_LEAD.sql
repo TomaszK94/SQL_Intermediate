1c4209278a9642de5d1d2efaa7c741fdb6012c97
TomaszK94 <pythonbotw3c@gmail.com>
2025-08-04 22:42:43 +0200

    LAG_LEAD_2

--- 5_Windows_Function/LAG_LEAD.sql
+++ 5_Windows_Function/LAG_LEAD.sql
@@ -1,9 +1,17 @@
-SELECT 
-    TO_CHAR(orderdate, 'YYYY-MM') AS month,
-    SUM(exchangerate * quantity * netprice) AS net_revenue
-FROM
-    sales
-GROUP BY 
-    month
-ORDER BY
-    month
+WITH monthly_revenue AS(
+    SELECT 
+        TO_CHAR(orderdate, 'YYYY-MM') AS month,
+        SUM(exchangerate * quantity * netprice) AS net_revenue
+    FROM
+        sales
+    WHERE
+        EXTRACT(YEAR FROM orderdate) = 2023
+    GROUP BY 
+        month
+    ORDER BY
+        month
+)
+
+SELECT *
+FROM 
+    monthly_revenue
