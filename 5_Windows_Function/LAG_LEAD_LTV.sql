2536f452d9c072ebe5edd1cfcca6b838d4e220c7
TomaszK94 <pythonbotw3c@gmail.com>
2025-08-05 23:14:00 +0200

    LAG_LEAD_LTV_2

--- 5_Windows_Function/LAG_LEAD_LTV.sql
+++ 5_Windows_Function/LAG_LEAD_LTV.sql
@@ -1,8 +1,20 @@
+WITH yearly_cohort AS(
+    SELECT
+        customerkey,
+        EXTRACT(YEAR FROM MIN(orderdate)) AS cohort_year,
+        SUM(quantity * netprice * exchangerate) AS customer_ltv
+    FROM
+        sales
+    GROUP BY
+        customerkey
+)
 SELECT
+    cohort_year,
     customerkey,
-    EXTRACT(YEAR FROM MIN(orderdate)) AS cohort_year,
-    SUM(quantity * netprice * exchangerate) AS customer_ltv
+    customer_ltv,
+    AVG(customer_ltv) OVER(PARTITION BY cohort_year) AS avg_cohort_ltv
 FROM
-    sales
-GROUP BY
+    yearly_cohort
+ORDER BY
+    cohort_year,
     customerkey
