6de688bf9df3a990b3d5ff93031c28e366942aec
TomaszK94 <pythonbotw3c@gmail.com>
2025-08-07 11:51:59 +0200

    ROWS_PRECEDING_1

--- 5_Windows_Function/N_PRECIDING&N_FOLLOWING.sql
+++ 5_Windows_Function/N_PRECIDING&N_FOLLOWING.sql
@@ -18,6 +18,14 @@ SELECT
     AVG(net_revenue) OVER (
         ORDER BY month
         ROWS BETWEEN 1 PRECEDING AND CURRENT ROW
-    ) AS net_revenue_preceding_1
+    ) AS net_revenue_preceding_1,
+       AVG(net_revenue) OVER (
+        ORDER BY month
+        ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+    ) AS net_revenue_preceding_2,
+       AVG(net_revenue) OVER (
+        ORDER BY month
+        ROWS BETWEEN 3 PRECEDING AND CURRENT ROW
+    ) AS net_revenue_preceding_3
 FROM
     monthly_sales
