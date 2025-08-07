ddb6b17d23e575eee83588942c9c3d389179dd9d
TomaszK94 <pythonbotw3c@gmail.com>
2025-08-07 11:59:35 +0200

    UNBOUNDED_1

---
+++ 5_Windows_Function/UNBOUNDED_PRECEDING&FOLLOWING.sql
@@ -0,0 +1,23 @@
+WITH monthly_sales AS( 
+    SELECT
+        TO_CHAR(orderdate, 'YYYY-MM') AS month,
+        SUM(quantity * netprice * exchangerate) AS net_revenue
+    FROM
+        sales
+    WHERE
+        EXTRACT(YEAR FROM orderdate) = 2023
+    GROUP BY
+        month 
+    ORDER BY
+        month
+)
+
+SELECT
+    month,
+    net_revenue,
+    AVG(net_revenue) OVER (
+        ORDER BY month
+        ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+    ) AS net_revenue_all_month
+FROM
+    monthly_sales
