dd12fdc640a041d041405dd4b355f809b6394c38
TomaszK94 <pythonbotw3c@gmail.com>
2025-08-07 11:50:47 +0200

    ROWS_PRECEDING_0

---
+++ 5_Windows_Function/N_PRECIDING&N_FOLLOWING.sql
@@ -0,0 +1,23 @@
+WITH monthly_sales AS( 
+    SELECT
+        TO_CHAR(orderdate, 'YYYY-MM') AS month,
+        SUM(quantity * netprice * exchangerate) AS net_revenue
+    FROM
+        sales
+    WHERE
+        EXTRACT(YEAR FROM orderdate) = 2023
+    GROUP BY
+        month 
+    ORDER BY
+        month
+)
+
+SELECT
+    month,
+    net_revenue,
+    AVG(net_revenue) OVER (
+        ORDER BY month
+        ROWS BETWEEN 1 PRECEDING AND CURRENT ROW
+    ) AS net_revenue_preceding_1
+FROM
+    monthly_sales
