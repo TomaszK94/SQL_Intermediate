c2b130defe35044792bd00245beaef74b4bc2692
TomaszK94 <pythonbotw3c@gmail.com>
2025-08-10 17:18:23 +0200

    Create_View_3

--- 7_PROJECT/0_Create_View.sql
+++ 7_PROJECT/0_Create_View.sql
@@ -1,20 +1,26 @@
-SELECT 
-    s.customerkey,
-    s.orderdate,
-    SUM(s.exchangerate * s.quantity * s.netprice) AS total_net_revenue,
-    COUNT(s.orderkey),
-    c.countryfull,
-    c.age,
-    c.givenname,
-    c.surname
+WITH customer_revenue AS(
+    SELECT 
+        s.customerkey,
+        s.orderdate,
+        SUM(s.exchangerate * s.quantity * s.netprice) AS total_net_revenue,
+        COUNT(s.orderkey),
+        c.countryfull,
+        c.age,
+        c.givenname,
+        c.surname
+    FROM
+        sales s
+    LEFT JOIN customer c 
+        ON c.customerkey = s.customerkey
+    GROUP BY
+        s.customerkey,
+        s.orderdate,
+        c.countryfull,
+        c.age,
+        c.givenname,
+        c.surname
+)
+
+SELECT *
 FROM
-    sales s
-LEFT JOIN customer c 
-    ON c.customerkey = s.customerkey
-GROUP BY
-    s.customerkey,
-    s.orderdate,
-    c.countryfull,
-    c.age,
-    c.givenname,
-    c.surname
+    customer_revenue
