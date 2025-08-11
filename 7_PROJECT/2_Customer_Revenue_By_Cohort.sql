dc1b6d16d301a85feba28ae3d62f5ef0b226f36f
TomaszK94 <pythonbotw3c@gmail.com>
2025-08-11 20:21:56 +0200

    2_Project_6

--- 7_PROJECT/2_Customer_Revenue_By_Cohort.sql
+++ 7_PROJECT/2_Customer_Revenue_By_Cohort.sql
@@ -30,3 +30,18 @@ GROUP BY
     days_since_first_purchase
 ORDER BY
     days_since_first_purchase
+
+
+-- Taking into consideration only first purchase
+
+SELECT
+    cohort_year,
+    COUNT(DISTINCT customerkey) AS total_customers,
+    SUM(total_net_revenue) AS total_revenue,
+    SUM(total_net_revenue) / COUNT(DISTINCT customerkey) AS customer_revenue
+FROM
+    cohort_analysis
+WHERE
+    orderdate = first_purchase_date
+GROUP BY
+    cohort_year;
