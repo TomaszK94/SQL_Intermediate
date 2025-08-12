a25958a000f02fe852577fefbb9ef1b596c1a95a
TomaszK94 <pythonbotw3c@gmail.com>
2025-08-12 22:45:52 +0200

    1_Conditional_Expressions

---
+++ 8_Data_Cleaning/Conditional_Expressions.sql
@@ -0,0 +1,15 @@
+/*
+COALESCE()
+
+Return the first non-null value from a list of expressions.
+
+SYNTAX:
+
+SELECT 
+    COALESCE(expression1, expression2, ..., default_value);
+
+
+Used to replace NULL values with a default. Common in reporting and data cleaning,
+ such as filtering missing values with a placeholder.
+
+*/
