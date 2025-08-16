edb584b2eb12e5ed2f0b604d343699277b16c7e5
TomaszK94 <pythonbotw3c@gmail.com>
2025-08-16 16:05:46 +0200

    1_Project_2

--- 7_PROJECT/1_Customer_Segmentation.sql
+++ 7_PROJECT/1_Customer_Segmentation.sql
@@ -1,11 +1,17 @@
-SELECT 
-    customerkey,
-    cleaned_name,
-    SUM(total_net_revenue) AS total_ltv
+WITH customer_ltv AS (
+    SELECT 
+        customerkey,
+        cleaned_name,
+        SUM(total_net_revenue) AS total_ltv
+    FROM
+        cohort_analysis
+    GROUP BY
+        customerkey,
+        cleaned_name
+    ORDER BY   
+        customerkey
+)
+
+SELECT *
 FROM
-    cohort_analysis
-GROUP BY
-    customerkey,
-    cleaned_name
-ORDER BY   
-    customerkey
+    customer_ltv
