24bab2008de5c51bd26da9d4bdbf012724446674
TomaszK94 <pythonbotw3c@gmail.com>
2025-08-16 16:02:00 +0200

    1_Project_1

---
+++ 7_PROJECT/1_Customer_Segmentation.sql
@@ -0,0 +1,11 @@
+SELECT 
+    customerkey,
+    cleaned_name,
+    SUM(total_net_revenue) AS total_ltv
+FROM
+    cohort_analysis
+GROUP BY
+    customerkey,
+    cleaned_name
+ORDER BY   
+    customerkey
