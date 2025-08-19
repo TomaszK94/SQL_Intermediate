cb7561695f86fd6b171d531239efbb3caa9cde5a
TomaszK94 <pythonbotw3c@gmail.com>
2025-08-19 22:19:08 +0200

    2_Optimization

---
+++ 8_Optimalization/2_Optimization.sql
@@ -0,0 +1,8 @@
+/*
+
+FUNDAMENTAL OPTIMIZATIONS:
+- Avoid SELECT * - Retrieve only necessary columns.
+- Use LIMIT for large datasets - Improve performance on large queries.
+- Use WHERE instead of HAVING - Filter before aggregation for efficiency.
+
+*/
