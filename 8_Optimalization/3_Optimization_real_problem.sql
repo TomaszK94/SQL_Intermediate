2bb85315bc1f1b91594cbe9ae852e84008a0ae98
TomaszK94 <pythonbotw3c@gmail.com>
2025-08-20 13:32:03 +0200

    3_Optimization_exe

--- 8_Optimalization/3_Optimization_real_problem.sql
+++ 8_Optimalization/3_Optimization_real_problem.sql
@@ -35,20 +35,16 @@ EXPLAIN ANALYZE
             s.orderdate,
             SUM(s.quantity * s.netprice / s.exchangerate) AS total_net_revenue,
             COUNT(s.orderkey) AS num_orders,
-            c.countryfull,
-            c.age,
-            CONCAT(TRIM(c.givenname),' ', TRIM(c.surname)) AS cleaned_name
+            MAX(c.countryfull) AS countryfull, --
+            MAX(c.age) AS age, --
+            MAX(CONCAT(TRIM(c.givenname),' ', TRIM(c.surname))) AS cleaned_name --
         FROM
             sales s
         INNER JOIN customer c  -- Changed join type to improve optimization
             ON c.customerkey = s.customerkey
         GROUP BY
             s.customerkey,
-            s.orderdate,
-            c.countryfull,
-            c.age,
-            c.givenname,
-            c.surname
+            s.orderdate  -- Replace GROUP BY into functions in SELECT statment
     )
     SELECT 
         cr.*,
