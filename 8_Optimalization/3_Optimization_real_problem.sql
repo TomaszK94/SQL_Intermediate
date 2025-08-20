fd9ffa1b7d69a1a4f87b25441ce25080135988f8
TomaszK94 <pythonbotw3c@gmail.com>
2025-08-20 13:26:30 +0200

    2_Optimization_exe

--- 8_Optimalization/3_Optimization_real_problem.sql
+++ 8_Optimalization/3_Optimization_real_problem.sql
@@ -26,3 +26,33 @@ EXPLAIN ANALYZE
         EXTRACT(YEAR FROM MIN(cr.orderdate) OVER(PARTITION BY cr.customerkey)) AS cohort_year
     FROM
         customer_revenue cr
+
+-- OPTIMIZE QUERY 
+EXPLAIN ANALYZE
+    WITH customer_revenue AS(
+        SELECT 
+            s.customerkey,
+            s.orderdate,
+            SUM(s.quantity * s.netprice / s.exchangerate) AS total_net_revenue,
+            COUNT(s.orderkey) AS num_orders,
+            c.countryfull,
+            c.age,
+            CONCAT(TRIM(c.givenname),' ', TRIM(c.surname)) AS cleaned_name
+        FROM
+            sales s
+        INNER JOIN customer c  -- Changed join type to improve optimization
+            ON c.customerkey = s.customerkey
+        GROUP BY
+            s.customerkey,
+            s.orderdate,
+            c.countryfull,
+            c.age,
+            c.givenname,
+            c.surname
+    )
+    SELECT 
+        cr.*,
+        MIN(cr.orderdate) OVER(PARTITION BY cr.customerkey) AS first_purchase_date,
+        EXTRACT(YEAR FROM MIN(cr.orderdate) OVER(PARTITION BY cr.customerkey)) AS cohort_year
+    FROM
+        customer_revenue cr
