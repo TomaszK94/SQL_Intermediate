bb16655b852117257dd52b132bae38219f66cad7
TomaszK94 <pythonbotw3c@gmail.com>
2025-08-21 13:31:51 +0200

    3_Project_3

--- 7_PROJECT/3_Retention_Analysis.sql
+++ 7_PROJECT/3_Retention_Analysis.sql
@@ -1,8 +1,14 @@
-SELECT 
-    customerkey,
-    cleaned_name,
-    orderdate,
-    ROW_NUMBER() OVER (PARTITION BY customerkey ORDER BY orderdate DESC) AS rn,
-    first_purchase_date
+WITH customer_last_purchase AS(
+    SELECT 
+        customerkey,
+        cleaned_name,
+        orderdate,
+        ROW_NUMBER() OVER (PARTITION BY customerkey ORDER BY orderdate DESC) AS rn,
+        first_purchase_date
+    FROM
+        cohort_analysis
+)
+
+SELECT *
 FROM
-    cohort_analysis
+    customer_last_purchase
