6ca6911ba2b18f88d0467f6848876abff7f2317e
TomaszK94 <pythonbotw3c@gmail.com>
2025-08-22 23:19:51 +0200

    17_Project_3

--- 7_PROJECT/3_Retention_Analysis.sql
+++ 7_PROJECT/3_Retention_Analysis.sql
@@ -69,3 +69,46 @@ FROM
     churned_customers
 GROUP BY 
     customer_status
+
+
+--
+
+WITH customer_last_purchase AS(
+    SELECT 
+        customerkey,
+        cleaned_name,
+        orderdate,
+        ROW_NUMBER() OVER (PARTITION BY customerkey ORDER BY orderdate DESC) AS rn,
+        first_purchase_date,
+        cohort_year
+    FROM
+        cohort_analysis
+), churned_customers AS(
+
+SELECT 
+    customerkey,
+    cleaned_name,
+    orderdate AS last_purchase_date,
+    CASE
+        WHEN orderdate < (SELECT MAX(orderdate) FROM sales) - INTERVAL '6 months' THEN 'Churned'
+        ELSE 'Active' 
+        END AS customer_status,
+    cohort_year
+FROM
+    customer_last_purchase
+WHERE
+    rn = 1 AND
+    first_purchase_date < (SELECT MAX(orderdate) FROM sales) - INTERVAL '6 months'
+)
+
+SELECT 
+    customer_status,
+    cohort_year,
+    -- COUNT(customerkey),
+    -- SUM(COUNT(customerkey)) OVER () AS total_customers,
+    ROUND(COUNT(customerkey) / SUM(COUNT(customerkey)) OVER() * 100, 2) AS percentage_customers
+FROM
+    churned_customers
+GROUP BY 
+    cohort_year,
+    customer_status
