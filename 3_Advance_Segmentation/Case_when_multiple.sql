f23034ebdddf6533d6bebb626b9fb65ef719750b
TomaszK94 <pythonbotw3c@gmail.com>
2025-08-25 17:13:20 +0200

    3_Exchangerate_fix

--- 3_Advance_Segmentation/Case_when_multiple.sql
+++ 3_Advance_Segmentation/Case_when_multiple.sql
@@ -1,8 +1,8 @@
 
 WITH percentile AS (
     SELECT
-        PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY (s.quantity * s.netprice * s.exchangerate)) AS perc_25th,
-        PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY (s.quantity * s.netprice * s.exchangerate)) AS perc_75th
+        PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY (s.quantity * s.netprice / s.exchangerate)) AS perc_25th,
+        PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY (s.quantity * s.netprice / s.exchangerate)) AS perc_75th
     FROM 
         sales AS s 
     WHERE
@@ -12,11 +12,11 @@ WITH percentile AS (
 SELECT
     product.categoryname,
     CASE
-        WHEN (sales.quantity * sales.netprice * sales.exchangerate) <= percentile.perc_25th THEN '3 - LOW'
-        WHEN (sales.quantity * sales.netprice * sales.exchangerate) >= percentile.perc_75th THEN '1 - HIGH'
+        WHEN (sales.quantity * sales.netprice / sales.exchangerate) <= percentile.perc_25th THEN '3 - LOW'
+        WHEN (sales.quantity * sales.netprice / sales.exchangerate) >= percentile.perc_75th THEN '1 - HIGH'
         ELSE '2 - MEDIUM'
     END AS revenu_tier,
-    SUM(sales.quantity * sales.netprice * sales.exchangerate) AS total_revenue
+    SUM(sales.quantity * sales.netprice / sales.exchangerate) AS total_revenue
 FROM
     sales
 LEFT JOIN 
--- 3_Advance_Segmentation/multiple_when_plus_and.sql
+++ 3_Advance_Segmentation/multiple_when_plus_and.sql
@@ -1,5 +1,5 @@
 SELECT
-    PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY (s.quantity * s.netprice * s.exchangerate)) AS median
+    PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY (s.quantity * s.netprice / s.exchangerate)) AS median
 FROM 
     sales AS s 
 WHERE
@@ -7,18 +7,18 @@ WHERE
 
 SELECT
     product.categoryname,
-    SUM(CASE WHEN (sales.quantity * sales.netprice * sales.exchangerate) < 398 
+    SUM(CASE WHEN (sales.quantity * sales.netprice / sales.exchangerate) < 398 
             AND sales.orderdate BETWEEN '2022-01-01' AND '2022-12-31'
-        THEN (sales.quantity * sales.netprice * sales.exchangerate) END) AS low_net_revenue_2022,
-    SUM(CASE WHEN (sales.quantity * sales.netprice * sales.exchangerate) >= 398 
+        THEN (sales.quantity * sales.netprice / sales.exchangerate) END) AS low_net_revenue_2022,
+    SUM(CASE WHEN (sales.quantity * sales.netprice / sales.exchangerate) >= 398 
             AND sales.orderdate BETWEEN '2022-01-01' AND '2022-12-31'
-        THEN (sales.quantity * sales.netprice * sales.exchangerate) END) AS high_net_revenue_2022,
-    SUM(CASE WHEN (sales.quantity * sales.netprice * sales.exchangerate) < 398 
+        THEN (sales.quantity * sales.netprice / sales.exchangerate) END) AS high_net_revenue_2022,
+    SUM(CASE WHEN (sales.quantity * sales.netprice / sales.exchangerate) < 398 
             AND sales.orderdate BETWEEN '2023-01-01' AND '2023-12-31'
-        THEN (sales.quantity * sales.netprice * sales.exchangerate) END) AS low_net_revenue_2023,
-    SUM(CASE WHEN (sales.quantity * sales.netprice * sales.exchangerate) >= 398 
+        THEN (sales.quantity * sales.netprice / sales.exchangerate) END) AS low_net_revenue_2023,
+    SUM(CASE WHEN (sales.quantity * sales.netprice / sales.exchangerate) >= 398 
             AND sales.orderdate BETWEEN '2023-01-01' AND '2023-12-31'
-        THEN (sales.quantity * sales.netprice * sales.exchangerate) END) AS high_net_revenue_2023
+        THEN (sales.quantity * sales.netprice / sales.exchangerate) END) AS high_net_revenue_2023
 FROM
     sales
 LEFT JOIN product ON sales.productkey = product.productkey
@@ -32,7 +32,7 @@ ORDER BY
 
 WITH median_value_2022_2023 AS (
     SELECT
-        PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY (s.quantity * s.netprice * s.exchangerate)) AS median
+        PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY (s.quantity * s.netprice / s.exchangerate)) AS median
     FROM 
     sales AS s 
     WHERE
@@ -41,18 +41,18 @@ WITH median_value_2022_2023 AS (
 
 SELECT
     product.categoryname,
-    SUM(CASE WHEN (sales.quantity * sales.netprice * sales.exchangerate) < mv.median
+    SUM(CASE WHEN (sales.quantity * sales.netprice / sales.exchangerate) < mv.median
             AND sales.orderdate BETWEEN '2022-01-01' AND '2022-12-31'
-        THEN (sales.quantity * sales.netprice * sales.exchangerate) END) AS low_net_revenue_2022,
-    SUM(CASE WHEN (sales.quantity * sales.netprice * sales.exchangerate) >= mv.median 
+        THEN (sales.quantity * sales.netprice / sales.exchangerate) END) AS low_net_revenue_2022,
+    SUM(CASE WHEN (sales.quantity * sales.netprice / sales.exchangerate) >= mv.median 
             AND sales.orderdate BETWEEN '2022-01-01' AND '2022-12-31'
-        THEN (sales.quantity * sales.netprice * sales.exchangerate) END) AS high_net_revenue_2022,
-    SUM(CASE WHEN (sales.quantity * sales.netprice * sales.exchangerate) < mv.median
+        THEN (sales.quantity * sales.netprice / sales.exchangerate) END) AS high_net_revenue_2022,
+    SUM(CASE WHEN (sales.quantity * sales.netprice / sales.exchangerate) < mv.median
             AND sales.orderdate BETWEEN '2023-01-01' AND '2023-12-31'
-        THEN (sales.quantity * sales.netprice * sales.exchangerate) END) AS low_net_revenue_2023,
-    SUM(CASE WHEN (sales.quantity * sales.netprice * sales.exchangerate) >= mv.median
+        THEN (sales.quantity * sales.netprice / sales.exchangerate) END) AS low_net_revenue_2023,
+    SUM(CASE WHEN (sales.quantity * sales.netprice / sales.exchangerate) >= mv.median
             AND sales.orderdate BETWEEN '2023-01-01' AND '2023-12-31'
-        THEN (sales.quantity * sales.netprice * sales.exchangerate) END) AS high_net_revenue_2023
+        THEN (sales.quantity * sales.netprice / sales.exchangerate) END) AS high_net_revenue_2023
 FROM
     sales
 LEFT JOIN 
